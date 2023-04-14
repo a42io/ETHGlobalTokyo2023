1d8a314fcf539ebaf60bd8247d10101fc1e41f65
Hideyoshi Moriya <hide@piyo.jp>
2023-04-14 23:19:49 +0900

    Discover NFC card #3

--- MynaSigner/app/src/main/java/dev/a42/mynasigner/MainActivity.kt
+++ MynaSigner/app/src/main/java/dev/a42/mynasigner/MainActivity.kt
@@ -1,11 +1,35 @@
 package dev.a42.mynasigner
 
+import android.nfc.NfcAdapter
+import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
-class MainActivity : AppCompatActivity() {
+private const val TAG = "Main"
+
+class MainActivity : AppCompatActivity(), NfcAdapter.ReaderCallback {
+
+    private lateinit var nfcAdapter: NfcAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        testNFC()
+    }
+
+    fun testNFC() {
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this.applicationContext)
+        nfcAdapter.enableReaderMode(
+            this,
+            this,
+            NfcAdapter.FLAG_READER_NFC_B or NfcAdapter.FLAG_READER_SKIP_NDEF_CHECK,
+            null
+        )
+    }
+
+    override fun onTagDiscovered(tag: Tag?) {
+        if (tag == null) return
+        Log.d(TAG, "onTagDiscovered")
     }
 }
