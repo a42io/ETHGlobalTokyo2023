8c7ab38029426f5b414ebd9c013cc464df44b54e
Hideyoshi Moriya <hide@piyo.jp>
2023-04-14 21:20:48 +0900

    Update content.js

--- playground/extention/content.js
+++ playground/extention/content.js
@@ -3,3 +3,9 @@ console.log("piyo");
 function hello() {
     console.log("hello");
 }
+
+var ethereum = {};
+ethereum.request = function(message) {
+    console.log(message);
+    alert("We need to write wallet connection logic!")
+}
