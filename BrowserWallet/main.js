7668ad6cd4dd1ae9c0b9f9f75f0ce03e72f62d19
Hideyoshi Moriya <hide@piyo.jp>
2023-04-16 04:42:55 +0900

    wip

--- BrowserWallet/main.js
+++ BrowserWallet/main.js
@@ -1,12 +1,17 @@
-const SERVICE_UUID = '5F155D9E-CBC8-448D-B29B-EA686D158842'.toLowerCase()
-const CHARCTERISTIC_UUID = '1B1D7724-59E0-41CF-BBAE-358999363D32'.toLowerCase()
 
-const mynaSigner = {}
+
+const LOCAL_STORAGE_KEY_MODULUS = "LOCAL_STORAGE_KEY_MODULUS";
+const LOCAL_STORAGE_KEY_ADDRESS = "LOCAL_STORAGE_KEY_ADDRESS";
+
+const SERVICE_UUID = '5F155D9E-CBC8-448D-B29B-EA686D158842'.toLowerCase();
+const CHARCTERISTIC_UUID = '1B1D7724-59E0-41CF-BBAE-358999363D32'.toLowerCase();
+
+const mynaSigner = {};
 
 function toHexString(value) {
 	return Array.from(new Uint8Array(value.buffer), function(byte) {
 	  return ('0' + (byte & 0xFF).toString(16)).slice(-2)
-	}).join('').toLowerCase()
+	}).join('').toLowerCase();
 }
 
 function toArrayBuffer(hexString) {
@@ -21,21 +26,25 @@ async function connectMynaSigner() {
 		filters: [{
 			services: [SERVICE_UUID]
 		}]
-	})
-	console.log(mynaSigner.device)
+	});
 
-	const gattServer = await mynaSigner.device.gatt.connect()
+	mynaSigner.device.addEventListener(
+		'gattserverdisconnected', 
+		onMynaSignerDisconnected
+	);
+	
+	const gattServer = await mynaSigner.device.gatt.connect();
 
-	mynaSigner.service = await gattServer.getPrimaryService(SERVICE_UUID)
-	mynaSigner.characteristic = await mynaSigner.service.getCharacteristic(CHARCTERISTIC_UUID)
+	mynaSigner.service = await gattServer.getPrimaryService(SERVICE_UUID);
+	mynaSigner.characteristic = await mynaSigner.service.getCharacteristic(CHARCTERISTIC_UUID);
 
 	mynaSigner.characteristic.addEventListener(
 		'characteristicvaluechanged',
 		onCharacteristicValueChanged
-	)
+	);
 
 	mynaSigner.characteristic.startNotifications();
-	onMynaSignerConnect()
+	onMynaSignerConnected()
 }
 
 // call from button
@@ -55,22 +64,44 @@ async function reload() {
 	showNotification("Reloaded!");
 }
 
+// call from button
+async function reset() {
+	if (window.confirm("Are you sure?")) {
+		localStorage.clear();
+	}
+}
+
 function showNotification(messsage) {
 	$("#notification-body").text(messsage)
 	$("#notification").toast("show")
 }
 
-function onMynaSignerConnect() {
-	console.log("connected to Myna Signer");
-	showNotification("Connected to MynaSigner");
+function onMynaSignerConnected() {
+	console.log("Connected to Myna Signer");
+	showNotification("Connected to Myna Signer");
+}
+
+function onMynaSignerDisconnected() {
+	console.log("Disconnected from Myna Signer");
+	showNotification("Disconnected from Myna Signer");
 }
 
 async function requestModulusMode() {
-	await mynaSigner.characteristic.writeValueWithResponse(new Uint8Array([1]));
+	try {
+		await mynaSigner.characteristic.writeValueWithResponse(new Uint8Array([1]));
+	} catch (e) {
+		console.log(e)
+		onMynaSignerDisconnected()
+	}
 }
 
 async function requestSignatureMode() {
-	await mynaSigner.characteristic.writeValueWithResponse(new Uint8Array([2]));
+	try {
+		await mynaSigner.characteristic.writeValueWithResponse(new Uint8Array([2]));
+	} catch (e) {
+		console.log(e)
+		onMynaSignerDisconnected()
+	}
 }
 
 function onCharacteristicValueChanged (event) {
@@ -81,8 +112,7 @@ function onCharacteristicValueChanged (event) {
 
 $(window).on('load', async () => {
 	var toastElList = [].slice.call(document.querySelectorAll(".toast"));
-	var toastList = toastElList.map(function (toastEl) {
+	toastElList.map(function (toastEl) {
   		return new bootstrap.Toast(toastEl, null);
 	});
-	console.log(toastList)
 })
